1cc8f753406b7f66dc2a1992b946435850209767
fankeng <36032655+fankeng@users.noreply.github.com>
2018-02-05 10:53:36 +0800

    Update main.go

--- main.go
+++ main.go
@@ -52,6 +52,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
 					log.Print(err)
 				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImagemapMessage("https://screenshot.net/tw/j7z74t4", "https://screenshot.net/tw/j7z74t4", linebot.ImagemapBaseSize{1040, 1040}, linebot.NewMessageImagemapAction("URANAI!", linebot.ImagemapArea{520, 520, 520, 520}))).Do(); err != nil {
+					log.Print(err)
+				}
 	
 			}
 		}
